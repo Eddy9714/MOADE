ec29731f95ada6322c6b9d607c399174ae14244a
Eddy9714 <59251745+Eddy9714@users.noreply.github.com>
2021-06-17 13:30:22 +0200

    test

--- MOADE_DPFSPE/Main.cpp
+++ MOADE_DPFSPE/Main.cpp
@@ -16,8 +16,8 @@ int main(int argc, char* argv[])
 	unsigned short T = 10;
 	unsigned int numeroValutazioni = 100000 - (H + 1);
 	double Fmax = 1.;
-	double alphaMin = 0.3;
-	double alphaMax = 0.7;
+	double alphaMin = 0.4;
+	double alphaMax = 0.6;
 	string percorso;
 
 	switch (argc) {
