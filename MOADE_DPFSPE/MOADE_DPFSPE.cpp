60597fdd5b0b21b9574259898dbd6588500a9879
Eddy9714 <59251745+Eddy9714@users.noreply.github.com>
2021-06-18 22:01:37 +0200

    test

--- MOADE_DPFSPE/MOADE_DPFSPE.cpp
+++ MOADE_DPFSPE/MOADE_DPFSPE.cpp
@@ -42,11 +42,23 @@ void MOADE_DPFSPE::inizializzaPopolazione(vector<Individuo>& popolazione, unsign
 	popolazione[0].simili.reserve(T);
 	ENEH(popolazione[0], valutazioniEffettuate);
 
-	for (unsigned short i = 1; i < popolazione.size(); i++) {
-		popolazione[i].rappresentazione->modulo2->random();
-		popolazione[i].alpha = alphaMin + ((alphaMax - alphaMin) * (double)i / H);
-		popolazione[i].simili.reserve(T);
-		ENEH(popolazione[i], valutazioniEffettuate);
+	popolazione[popolazione.size() - 1].rappresentazione->modulo2->massimo();
+	popolazione[popolazione.size() - 1].alpha = alphaMax;
+	popolazione[popolazione.size() - 1].simili.reserve(T);
+	ENEH(popolazione[popolazione.size() - 1], valutazioniEffettuate);
+
+	for (unsigned short i = 1; i < popolazione.size() - 1; i++) {
+		if (i % 5 == 0) {
+			popolazione[i].rappresentazione->modulo2->random();
+			popolazione[i].alpha = alphaMin + ((alphaMax - alphaMin) * (double)i / H);
+			popolazione[i].simili.reserve(T);
+			ENEH(popolazione[i], valutazioniEffettuate);
+		}
+		else {
+			popolazione[i].rappresentazione->random();
+			popolazione[i].rappresentazione->modulo1->ordina();
+			valutaIndividuo(popolazione[i], valutazioniEffettuate);
+		}
 	}
 
 	int prima, dopo;
