0cf6f21b13d86578046b7ddf29626655bf5f2250
Eddy9714 <59251745+Eddy9714@users.noreply.github.com>
2021-06-19 14:15:09 +0200

    update

--- MOADE_DPFSPE/MOADE_DPFSPE.cpp
+++ MOADE_DPFSPE/MOADE_DPFSPE.cpp
@@ -38,13 +38,8 @@ void MOADE_DPFSPE::inizializzaPopolazione(vector<Individuo>& popolazione, unsign
 	GruppoPDZN* rappresentazione;
 
 	popolazione[0].rappresentazione->modulo2->identita();
-	popolazione[0].rappresentazione->permutazione->random();
-	popolazione[0].rappresentazione->modulo1->random();
-	popolazione[0].rappresentazione->modulo1->ordina();
-
 	popolazione[0].alpha = alphaMin;
 	popolazione[0].simili.reserve(T);
-	valutaIndividuo(popolazione[0], valutazioniEffettuate);
 	ENEH(popolazione[0], valutazioniEffettuate);
 
 	popolazione[popolazione.size() - 1].rappresentazione->modulo2->massimo();
